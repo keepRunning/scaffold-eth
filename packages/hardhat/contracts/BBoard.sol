4c9e9450c8c5018da26b8d057336ea9fc511bfc4
João Mateus <82168292+joaomatbarbosa@users.noreply.github.com>
2021-08-09 10:28:47 +0100

    Update BBoard.sol

--- packages/hardhat/contracts/BBoard.sol
+++ packages/hardhat/contracts/BBoard.sol
@@ -106,4 +106,27 @@ contract BBoard is ReentrancyGuard {
         idToBBlock[bblockId].sold = true;
         payable(owner).transfer(getBasefee());
     }
+    
+    function fetchMyNFTs() public view returns (BBlock[] memory) {
+        uint256 totalItemCount = _bblockIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToBBlock[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        BBlock[] memory items = new BBlock[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToBBlock[i + 1].owner == msg.sender) {
+                uint256 currentId = i + 1;
+                BBlock storage currentItem = idToBBlock[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
 }
