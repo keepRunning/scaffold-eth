19714c3ad7bebc36205068ae7755b4111b9aceca
tomosaigon <tomo@crowdeo.com>
2021-08-10 17:39:43 +0700

    Add a demo Material modal

--- packages/react-app/src/views/ExampleUI.jsx
+++ packages/react-app/src/views/ExampleUI.jsx
@@ -9,6 +9,7 @@ import AnsiImageRender from "./AnsiImageRender"
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Modal from '@material-ui/core/Modal';
 
 import { NFTStorage, File } from 'nft.storage';
 
@@ -33,6 +34,68 @@ async function nftsTestStoreString(s) {
   console.log('metadata.json with IPFS gateway URLs:\n', metadata.embed())
 }
 
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useModalStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+function SimpleModal() {
+
+  const classes = useModalStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Text in a modal</h2>
+      <p id="simple-modal-description">
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </p>
+      <SimpleModal />
+    </div>
+  );
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
 
 // copied from ansi
 const useStyles = makeStyles((theme) => ({
@@ -81,6 +144,7 @@ function RecentlySavedBlocks({limit}) {
           <div><span>Last known minted block at index 679.</span></div>
           <div><span>Mint cost for next block: 0.00125 MATIC</span></div>
           <button>MINT NEXT AVAILABLE BLOCK</button>
+          <SimpleModal />
         </Grid>
         <div style={{border: '1px solid black'}}>
           <h2 style={{fontFamily: '"Roboto", sans-serif', fontSize: '4em', textAlign: 'left', fontWeight: 800}} className='foobar'>Recently Saved Blocks ({limit})</h2>
@@ -131,6 +195,7 @@ function RecentlySavedBlocks({limit}) {
   )
 }
 function AutoGrid() {
+  return '';
   const classes = useStyles();
   const classBreadcrumbListItem = {
               display: 'inline-block',
